b8eae4976a9b3ffe4705862b2a4fe359e69ef665
Michał Piotrowski <misiulek321@gmail.com>
2019-02-25 22:10:28 +0100

    Clean after RTSP tests

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -23,20 +23,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/es6-promise-plugin/www/promise.js",
-        "id": "es6-promise-plugin.Promise",
-        "pluginId": "es6-promise-plugin",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-rtsp-vlc/www/VideoPlayerVLC.js",
-        "id": "cordova-plugin-rtsp-vlc.VideoPlayerVLC",
-        "pluginId": "cordova-plugin-rtsp-vlc",
-        "clobbers": [
-            "window.VideoPlayerVLC"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-insomnia/www/Insomnia.js",
         "id": "cordova-plugin-insomnia.Insomnia",
         "pluginId": "cordova-plugin-insomnia",
@@ -59,8 +45,6 @@ module.exports.metadata =
     "com.rjfun.cordova.httpd": "0.9.2",
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-secure-storage": "2.6.8",
-    "es6-promise-plugin": "4.1.0",
-    "cordova-plugin-rtsp-vlc": "3.0.4",
     "cordova-plugin-insomnia": "4.3.0"
 }
 // BOTTOM OF METADATA
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,20 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "es6-promise-plugin.Promise",
-    "file": "plugins/es6-promise-plugin/www/promise.js",
-    "pluginId": "es6-promise-plugin",
-    "runs": true
-  },
-  {
-    "id": "cordova-plugin-rtsp-vlc.VideoPlayerVLC",
-    "file": "plugins/cordova-plugin-rtsp-vlc/www/VideoPlayerVLC.js",
-    "pluginId": "cordova-plugin-rtsp-vlc",
-    "clobbers": [
-      "window.VideoPlayerVLC"
-    ]
-  },
-  {
     "id": "cordova-plugin-insomnia.Insomnia",
     "file": "plugins/cordova-plugin-insomnia/www/Insomnia.js",
     "pluginId": "cordova-plugin-insomnia",
@@ -26,8 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "es6-promise-plugin": "4.1.0",
-  "cordova-plugin-rtsp-vlc": "3.0.4",
   "cordova-plugin-insomnia": "4.3.0"
 };
 // BOTTOM OF METADATA
